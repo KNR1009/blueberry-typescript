dfef8248ec47dfc2568c97efe2f4fb15d6eb1fa4
伊東和成 <itokazunari5375@icloud.com>
2022-06-05 07:54:16 +0900

    変数の型定義

--- src/components/pages/TypeTest.tsx
+++ src/components/pages/TypeTest.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 
+type Family<Parent, Child> = {
+  mother: Parent;
+  father: Parent;
+  child: Child;
+};
 export const TypeTest = () => {
-  console.log("type script テストです");
+  const family: Family<String, Number> = {
+    mother: "aaa",
+    father: "bbb",
+    child: 0,
+  };
   return <div>TypeTest</div>;
 };
