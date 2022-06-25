1277bd57f951b6ffd5eb179baaf2341cf21548b9
伊東和成 <itokazunari5375@icloud.com>
2022-06-25 16:03:56 +0900

    一旦整頓

--- src/components/04_Functions/index.tsx
+++ src/components/04_Functions/index.tsx
@@ -1,17 +1,5 @@
 import React, { useState } from "react";
 
 export const Functions = () => {
-  type Single = "plus" | "minus";
-  const get = (param: "plus" | "minus") => {
-    return param === "plus" ? 1 : -1;
-  };
-
-  const test = (type: Single | "none") => {
-    if (type === "none") {
-      console.log("データなし");
-    } else {
-      get(type);
-    }
-  };
   return <div></div>;
 };
