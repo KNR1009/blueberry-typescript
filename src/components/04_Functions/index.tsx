c0cb66c22a70a55e621425d8aa49b797d5ef53ab
伊東和成 <itokazunari5375@icloud.com>
2022-06-25 13:04:28 +0900

    修正

--- src/components/04_Functions/index.tsx
+++ src/components/04_Functions/index.tsx
@@ -1,43 +1,17 @@
 import React, { useState } from "react";
 
 export const Functions = () => {
-  const [value1, setValue1] = useState<string>("");
-  const [value2, setValue2] = useState<string>("");
-  const [value3, setValue3] = useState<string>("");
-
-  const onChangeObj = {
-    value1: (e: React.ChangeEvent<HTMLInputElement>) =>
-      setValue1(e.target.value),
-    value2: (e: React.ChangeEvent<HTMLInputElement>) =>
-      setValue2(e.target.value),
-    value3: (e: React.ChangeEvent<HTMLInputElement>) =>
-      setValue3(e.target.value),
+  type Single = "plus" | "minus";
+  const get = (param: "plus" | "minus") => {
+    return param === "plus" ? 1 : -1;
   };
 
-  return (
-    <div>
-      <div className="input-container">
-        <label htmlFor="">入力フォーム1</label>
-        <input
-          type="text"
-          value={value1}
-          onChange={(e) => {
-            onChangeObj.value1(e);
-          }}
-        />
-        <label htmlFor="">入力フォーム2</label>
-        <input
-          type="text"
-          value={value2}
-          onChange={(e) => onChangeObj.value2(e)}
-        />
-        <label htmlFor="">入力フォーム3</label>
-        <input
-          type="text"
-          onChange={(e) => onChangeObj.value3(e)}
-          value={value3}
-        />
-      </div>
-    </div>
-  );
+  const test = (type: Single | "none") => {
+    if (type === "none") {
+      console.log("データなし");
+    } else {
+      get(type);
+    }
+  };
+  return <div></div>;
 };
