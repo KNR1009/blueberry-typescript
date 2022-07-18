a9112cd0ab07eeb6367787d8c2b493d9366422fc
伊東和成 <itokazunari5375@icloud.com>
2022-07-18 10:41:17 +0900

    obj型の定義

--- src/components/04_Functions/index.tsx
+++ src/components/04_Functions/index.tsx
@@ -1,10 +1,39 @@
 import React, { useState } from "react";
 
 export const Functions = () => {
-  const obj = {
-    name: "kazu",
-    age: 14,
+  const [name, setName] = useState<string>("");
+  const [age, setAge] = useState<number>();
+  const [isAdmin, setIsAdmin] = useState<boolean>();
+
+  const onChange = {
+    name: (e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value),
+    age: (e: React.ChangeEvent<HTMLSelectElement>) =>
+      setAge(Number(e.target.value)),
+    isAdmin: (e: React.ChangeEvent<HTMLInputElement>) =>
+      setIsAdmin(e.target.checked),
   };
-  const { name } = obj;
-  return <div></div>;
+
+  return (
+    <div>
+      <div className="input-container">
+        <label htmlFor="">名前</label>
+        <input type="text" onChange={onChange["name"]} value={name} />
+      </div>
+      <div className="select-container">
+        <label htmlFor="">年齢</label>
+        <select name="" id="" onChange={onChange["age"]}>
+          <option value="">24</option>
+          <option value="">25</option>
+        </select>
+      </div>
+      <div className="check-container">
+        <label htmlFor="">管理者</label>
+        <input
+          type="checkbox"
+          onChange={onChange["isAdmin"]}
+          checked={isAdmin}
+        />
+      </div>
+    </div>
+  );
 };
