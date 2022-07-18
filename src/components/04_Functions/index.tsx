16b1c7e74bf39d3a1bf4d53e8ba5783fee615b29
伊東和成 <itokazunari5375@icloud.com>
2022-07-18 09:38:51 +0900

    方の修正

--- src/components/04_Functions/index.tsx
+++ src/components/04_Functions/index.tsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 
 export const Functions = () => {
+  const obj = {
+    name: "kazu",
+    age: 14,
+  };
+  const { name } = obj;
   return <div></div>;
 };
