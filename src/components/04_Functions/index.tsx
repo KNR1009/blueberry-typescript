8bc20947e5ba126793f762afdc83f8afc51e6503
伊東和成 <itokazunari5375@icloud.com>
2022-07-23 15:40:48 +0900

    配列操作

--- src/components/04_Functions/index.tsx
+++ src/components/04_Functions/index.tsx
@@ -1,39 +1,12 @@
 import React, { useState } from "react";
 
 export const Functions = () => {
-  const [name, setName] = useState<string>("");
-  const [age, setAge] = useState<number>();
-  const [isAdmin, setIsAdmin] = useState<boolean>();
-
-  const onChange = {
-    name: (e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value),
-    age: (e: React.ChangeEvent<HTMLSelectElement>) =>
-      setAge(Number(e.target.value)),
-    isAdmin: (e: React.ChangeEvent<HTMLInputElement>) =>
-      setIsAdmin(e.target.checked),
+  const array = [1, 2, 3, 4, 5, 6];
+  // 偶数を抽出する関数
+  const getEvenNumber = (numArray: readonly number[]) => {
+    // 想定外に処理
+    return numArray.filter((i) => i % 2 === 0);
   };
-
-  return (
-    <div>
-      <div className="input-container">
-        <label htmlFor="">名前</label>
-        <input type="text" onChange={onChange["name"]} value={name} />
-      </div>
-      <div className="select-container">
-        <label htmlFor="">年齢</label>
-        <select name="" id="" onChange={onChange["age"]}>
-          <option value="">24</option>
-          <option value="">25</option>
-        </select>
-      </div>
-      <div className="check-container">
-        <label htmlFor="">管理者</label>
-        <input
-          type="checkbox"
-          onChange={onChange["isAdmin"]}
-          checked={isAdmin}
-        />
-      </div>
-    </div>
-  );
+  console.log(getEvenNumber(array));
+  return <p>test</p>;
 };
