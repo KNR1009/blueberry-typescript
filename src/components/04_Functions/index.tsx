c55255ea93e3bdc2ba65344d708493ed26b66dd2
伊東和成 <itokazunari5375@icloud.com>
2022-08-07 18:14:44 +0900

    修正

--- src/components/04_Functions/index.tsx
+++ src/components/04_Functions/index.tsx
@@ -1,12 +1,8 @@
+import { info } from "console";
 import React, { useState } from "react";
 
 export const Functions = () => {
-  const array = [1, 2, 3, 4, 5, 6];
-  // 偶数を抽出する関数
-  const getEvenNumber = (numArray: readonly number[]) => {
-    // 想定外に処理
-    return numArray.filter((i) => i % 2 === 0);
-  };
-  console.log(getEvenNumber(array));
+  //
+
   return <p>test</p>;
 };
